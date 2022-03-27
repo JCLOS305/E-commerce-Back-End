1050ba93c6a25fda2ae78aefc925bb1915837500
jclos305 <jclos305@hotmail.com>
2022-03-27 11:31:40 -0400

    tags updated and defined

--- models/Tag.js
+++ models/Tag.js
@@ -4,9 +4,20 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
+
+//  columns defined 
+
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
